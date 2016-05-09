ded9fc26b02e4730c1ad0dc620899d77ae1a26aa
yamadapc <tacla.yamada@gmail.com>
2016-05-09 17:45:02 -0300

    Initial Commit

---
+++ just-use-a-webview/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  just-use-a-webview
+//
+//  Created by Pedro Tacla Yamada on 5/9/16.
+//  Copyright © 2016 Pedro Tacla Yamada. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ just-use-a-webview/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  just-use-a-webview
+//
+//  Created by Pedro Tacla Yamada on 5/9/16.
+//  Copyright © 2016 Pedro Tacla Yamada. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
